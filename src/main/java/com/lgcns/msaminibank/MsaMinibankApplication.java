819e102b42393dd54fbf4778f02534ea9899f8a6
injaesong <injaesong0@gmail.com>
2025-01-23 16:53:10 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/lgcns/msaminibank/MsaMinibankApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.msaminibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaMinibankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaMinibankApplication.class, args);
+    }
+
+}
