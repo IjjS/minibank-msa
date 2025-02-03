4a21141c36cb3c74a5ac55fcdef1212dbc7004ef
injaesong <injaesong0@gmail.com>
2025-02-03 16:39:52 +0900

    feat: Open Feign 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
 }
+val springCloudVersion by extra("2024.0.0")
 
 group = "com.lgcns"
 version = "0.0.1-SNAPSHOT"
@@ -33,6 +34,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
 
+    // Open Feign
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
     // Eureka Client
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.2.0")
 
@@ -42,6 +46,11 @@ dependencies {
 
     // Swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.3")
+}
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
 }
 
 tasks.withType<Test> {
