ed102056905c2789dbb300144b60267ef64187b3
injaesong <injaesong0@gmail.com>
2025-03-07 21:15:02 +0900

    feat: 요청에 은행코드 추가 (DB 테이블로 추가 예정)

--- src/main/java/com/msa/minibanktransfer/dto/request/BankToBankTransferRequest.java
+++ src/main/java/com/msa/minibanktransfer/dto/request/BankToBankTransferRequest.java
@@ -5,10 +5,11 @@ import lombok.Builder;
 import java.math.BigDecimal;
 
 @Builder
-public record BankToBankTransferRequest(Long customerId, Long customerSequence, Long accountSequence, Long depositAccountNumber, Long withdrawalAccountNumber, String receiverMemo, String senderMemo, String receiverCustomerName, BigDecimal transferAmount, Boolean isFailed) {
+public record BankToBankTransferRequest(String bankCode, Long customerId, Long customerSequence, Long accountSequence, Long depositAccountNumber, Long withdrawalAccountNumber, String receiverMemo, String senderMemo, String receiverCustomerName, BigDecimal transferAmount, Boolean isFailed) {
 
     public static BankToBankTransferRequest from(TransferRequest request, Long customerSequence, Long accountSequence) {
         return BankToBankTransferRequest.builder()
+                .bankCode(request.bankCode())
                 .customerId(request.customerId())
                 .customerSequence(customerSequence)
                 .accountSequence(accountSequence)
