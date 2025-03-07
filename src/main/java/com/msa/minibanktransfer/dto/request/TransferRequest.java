913caa2efb6e2f30996e440f3a4c499daac8b6ab
injaesong <injaesong0@gmail.com>
2025-03-07 21:16:38 +0900

    feat: 은행코드 요청에 추가

--- src/main/java/com/msa/minibanktransfer/dto/request/TransferRequest.java
+++ src/main/java/com/msa/minibanktransfer/dto/request/TransferRequest.java
@@ -2,5 +2,5 @@ package com.msa.minibanktransfer.dto.request;
 
 import java.math.BigDecimal;
 
-public record TransferRequest(Long customerId, Long depositAccountNumber, Long withdrawalAccountNumber, String receiverMemo, String senderMemo, String receiverCustomerName, BigDecimal transferAmount, Boolean isFailed) {
+public record TransferRequest(String bankCode, Long customerId, Long depositAccountNumber, Long withdrawalAccountNumber, String receiverMemo, String senderMemo, String receiverCustomerName, BigDecimal transferAmount, Boolean isFailed) {
 }
