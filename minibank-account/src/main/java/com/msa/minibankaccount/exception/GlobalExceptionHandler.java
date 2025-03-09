90da8b371e4406b2d7a07dcb8dc263c13c5cb7a5
injaesong <injaesong0@gmail.com>
2025-03-09 21:58:16 +0900

    feat: 계정 서비스 시스템 예외 추가

--- minibank-account/src/main/java/com/msa/minibankaccount/exception/GlobalExceptionHandler.java
+++ minibank-account/src/main/java/com/msa/minibankaccount/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.msa.minibankaccount.exception;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,10 +12,10 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(new ErrorResponse(exception.getMessage()));
     }
 
-    @ExceptionHandler(HttpClientErrorException.class)
-    public ResponseEntity<ErrorResponse> handleRestClientException(HttpClientErrorException exception) {
-        return ResponseEntity.status(exception.getStatusCode())
-                .body(exception.getResponseBodyAs(ErrorResponse.class));
+    @ExceptionHandler(SystemException.class)
+    public ResponseEntity<ErrorResponse> handleSystemException(SystemException exception) {
+        return ResponseEntity.internalServerError()
+                .body(new ErrorResponse(exception.getMessage()));
     }
 
 }
---
+++ minibank-account/src/main/java/com/msa/minibankaccount/exception/SystemException.java
@@ -0,0 +1,9 @@
+package com.msa.minibankaccount.exception;
+
+public class SystemException extends RuntimeException {
+
+    public SystemException(String message) {
+        super(message);
+    }
+
+}
